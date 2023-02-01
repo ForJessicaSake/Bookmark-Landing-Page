21feab6f8879547c1991104427ea11be1755b74d
Jessica Joseph <jessicajoseph2019@yahoo.com>
2023-02-01 04:22:24 +0100

    fix:made changes to the navigation bar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -7,10 +7,12 @@ const Navbar = () => {
   const [isLinksVisible, setLinksVisible] = useState(false);
 
   return (
-    <nav className="flex justify-between items-center px-4 lg:px-14 mt-10">
+    <nav className="lg:px-14 mt-10">
+      <section className="flex justify-between items-center px-4">
       <img src={logo} alt=""className=""/>
       <img src={menu} alt="Navigation menu" className=" w-6 h-4 lg:hidden cursor-pointer" onClick={() => setLinksVisible(!isLinksVisible)} />
-      <section className={`${isLinksVisible ? 'block' : 'hidden'} lg:static bg-Blue h-5/6  lg:h-0 flex flex-col lg:flex-row justify-center items-center lg:bg-white absolute top-24  lg:w-5/12 pr-20 z-50 lg:flex lg:items-center`}>
+      </section>
+      <section className={`${isLinksVisible ? 'block' : 'hidden'} lg:static bg-Blue h-5/6 lg:h-0 flex flex-col lg:flex-row justify-center items-center lg:bg-white absolute top-24  lg:w-5/12 pr-20 z-50 lg:flex lg:items-center`}>
         <ul className="flex w-screen lg:flex-row flex-col h-80 mb-10 lg:mb-0 items-center lg:h-0 justify-between lg:w-72 text-xs font-medium text-downText lg:text-Ash">
           <li>FEATURES</li>
           <li>PRICING</li>
