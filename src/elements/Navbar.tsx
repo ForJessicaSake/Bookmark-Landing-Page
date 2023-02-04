bb25ddb6915c34de637c300f7789e6062f8d64e6
Jessica Joseph <jessicajoseph2019@yahoo.com>
2023-02-04 14:19:00 +0100

    fix:navbar close feature

--- src/elements/Navbar.tsx
+++ src/elements/Navbar.tsx
@@ -5,6 +5,9 @@ import CallToAction from "../components/button/CallToAction";
 
 const Navbar = () => {
   const [isLinksVisible, setLinksVisible] = useState(false);
+  const handleClose =()=>{
+    setLinksVisible(false)
+  }
 
   return (
     <nav className="lg:px-14 pt-8 lg:flex lg:justify-between lg:items-center cursor-pointer">
@@ -14,9 +17,9 @@ const Navbar = () => {
       </section>
       <section className={`${isLinksVisible ? 'block' : 'hidden'} lg:static bg-Blue h-5/6 lg:h-0 flex flex-col lg:flex-row justify-center items-center lg:bg-white absolute top-24  lg:w-5/12 pr-20 z-50 lg:flex lg:items-center`}>
         <ul className="flex w-screen lg:flex-row flex-col h-80 mb-10 lg:mb-0 items-center lg:h-0 justify-between lg:w-72 text-base lg:text-xs font-medium text-downText lg:text-Ash">
-        <a href="#features"><li>FEATURES</li></a>  
-        <a href="#hero"><li>PRICING</li></a>  
-        <a href="#contact"><li>CONTACT</li></a>  
+        <a href="#features" onClick={handleClose}><li>FEATURES</li></a>  
+        <a href="#hero" onClick={handleClose}><li>PRICING</li></a>  
+        <a href="#contact" onClick={handleClose}><li>CONTACT</li></a>  
           <li className="block lg:hidden">LOGIN</li>
         </ul>
         <section className="text-white text-xs hidden lg:block">
